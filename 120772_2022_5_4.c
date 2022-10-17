fd28f409fb8152f20c93ce11e810a579883de584
Kristián Červenka <kristian.cervenka@gmail.com>
2022-10-17 20:22:12 +0200

    dokoncene

--- 120772_2022_5_4.c
+++ 120772_2022_5_4.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-
+void vymen(int *, int *);
 int main()
 {
-    printf("Hello world\n");
+    int a = 10,b = 14;
+    printf("p_a: %p %d\n",&a,a);
+    printf("p_b: %p %d\n",&b,b);
+    vymen(&a,&b);
+    printf("p_a: %p %d\n",&a,a);
+    printf("p_b: %p %d\n",&b,b); 
     return 0;
+}
+void vymen(aa,bb)
+int *aa,*bb;
+{
+    int sw;
+    sw = *aa;
+    *aa = *bb;
+    *bb =sw;
 }
--- 120772_2022_5_5.c
+++ 120772_2022_5_5.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
-
+int delitele(int x[], int pocetx, int y[], int k)
+{
+    int sucet;
+    int l=0;
+    int i = 0;
+    for(i=0; i < pocetx;i++)
+    {
+        if(x[i] != 0)
+        {
+            if(k%x[i]==0)
+            {
+                y[l] = x[i];
+                l++;
+                sucet++;
+            }
+        }
+    }
+    return sucet;
+}
 int main()
 {
-    printf("Hello world\n");
+    int x[10],pocetx;
+    int y[10];
+    int i = 0;
+    scanf("%d",&pocetx);
+    for(i = 0; i < pocetx;i++)
+        scanf("%d",&x[i]);
+    int pocety = delitele(x,pocetx,y,24);
+    printf("pocety: %d\ny: {",pocety);
+    for(i=0;i < pocety;i++)
+    {
+        if(i > 0)
+            printf(", ");
+        printf("%d",y[i]);
+    }
+    printf("}\n");
+
+    
     return 0;
 }
